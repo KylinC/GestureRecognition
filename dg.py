3dd175bf9360a3c3e99c6d3baceed9ff51939a71
Arthur-99 <1224795746@qq.com>
2019-11-24 19:11:49 +0800

    data generator

---
+++ dg.py
@@ -0,0 +1,27 @@
+# dg refers to data-generator
+
+import numpy as np
+import cv2
+from cpp import CPP
+from dip import *
+
+cpp = CPP()
+cpp.start()
+
+try:
+    while cv2.waitKey(1) != ord('q'):
+        color, depth = cpp.cd()
+        show_cd(color, depth, 'cd')
+
+        # crop
+        hand_c, hand_d = get_hand_cd(color, depth)
+        if hand_c is None or hand_d is None:
+            # end of recording or not started
+            continue
+
+        show_cd(hand_c, hand_d, 'hand_cd')
+
+        data = np.dstack((hand_c, hand_d))
+
+finally:
+    cpp.stop()
