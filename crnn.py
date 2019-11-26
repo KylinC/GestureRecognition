d2f963d16bac378837bddde879fd15ed439b1a1b
Arthur-99 <1224795746@qq.com>
2019-11-26 17:25:48 +0800

    crnn frame

--- crnn.py
+++ crnn.py
@@ -1,3 +1,10 @@
 import torch 
 import torch.nn as nn 
 import torch.optim as optim
+
+class crnn(nn.Module):
+    def __init__(self):
+        super(crnn, self).__init__()
+
+    def forward(self, x):
+        pass 
