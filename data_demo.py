1205ba74511743c05b9fb0515a592b1853920d3d
Arthur-99 <1224795746@qq.com>
2019-11-26 16:57:10 +0800

    data demo

--- data_demo.py
+++ data_demo.py
@@ -3,12 +3,12 @@ import numpy as np
 import cv2 
 import os
 
-root_path = './data/'
+root_path = './data/wipe_left'
 
 for file_name in os.listdir(root_path):
     file_name = os.path.join(root_path, file_name)
     f = open(file_name, 'rb')
-    seq = pickle.load(f)
+    seq, label = pickle.load(f)
     for c, d in seq:
         cv2.imshow('c', c)
         cv2.waitKey(1)
