7b9a1335fe85f10fff0bd7d5ca27b97ec029a777
Arthur-99 <1224795746@qq.com>
2019-11-26 16:56:40 +0800

    fix bug: resizing 1-dim picture

--- bb.py
+++ bb.py
@@ -10,32 +10,37 @@ def get_bb(closed):
     try:
         c = sorted(cnts, key=cv2.contourArea,
                    reverse=True)[0]  # find the largest one
+
+        # compute the rotated bounding box of the largest contour
+        rect = cv2.minAreaRect(c)
+        box = np.int0(cv2.boxPoints(rect))
+
+        # get right box
+        Xs = [i[0] for i in box]
+        Ys = [i[1] for i in box]
+        x1 = min(Xs)
+        x2 = max(Xs)
+        y1 = min(Ys)
+        y2 = max(Ys)
+        height = y2 - y1
+        width = x2 - x1
+
+        bb = [
+            max(y1, 0),
+            min(y1 + height, closed.shape[0]),
+            max(x1, 0),
+            min(x1 + width, closed.shape[1])
+        ]
+
+        return bb  # [y_min, y_max, x_min, x_max]
+
     except:
         return None
 
-    # compute the rotated bounding box of the largest contour
-    rect = cv2.minAreaRect(c)
-    box = np.int0(cv2.boxPoints(rect))
-
-    # get right box
-    Xs = [i[0] for i in box]
-    Ys = [i[1] for i in box]
-    x1 = min(Xs)
-    x2 = max(Xs)
-    y1 = min(Ys)
-    y2 = max(Ys)
-    height = y2 - y1
-    width = x2 - x1
-
-    bb = [
-        max(y1, 0),
-        min(y1 + height, closed.shape[0]),
-        max(x1, 0),
-        min(x1 + width, closed.shape[1])
-    ]
-
-    return bb  # [y_min, y_max, x_min, x_max]
-
 
 def get_crop_image(image, bb):
-    return image[bb[0]:bb[1], bb[2]:bb[3]]
+    cropped = image[bb[0]:bb[1], bb[2]:bb[3]]
+    if cropped.ndim == 1:
+        cropped = cropped.reshape(cropped.shape[0], -1)
+
+    return cropped
