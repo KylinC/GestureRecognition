8bb261e10be2a81d6b57605084584dc65402047d
Arthur-99 <1224795746@qq.com>
2019-11-27 20:24:30 +0800

    demo file of gesture recognition

--- gr.py
+++ gr.py
@@ -9,20 +9,30 @@ from time import time
 
 from cpp import CPP
 import dip
+from pp import *
+from crnn import crnn
+
+model = crnn()
+model_path = 'model/model.pth'
+model.load_state_dict(torch.load('model/model.pth'))
+
+gesture_map = {}
+gesture_map_rev = {}
+for i, gesture in enumerate(os.listdir('data')):
+    gesture_map[gesture] = i
+    gesture_map_rev[i] = gesture
 
 cpp = CPP()
 cpp.start()
 
 root_path = './data/'
 
-if not os.path.exists(root_path):
-    os.makedirs(root_path)
+# if not os.path.exists(root_path):
+#     os.makedirs(root_path)
 
 recording = False
 
 try:
-    print("Please show me your hand pose, q to quit!")
-
     while cv2.waitKey(1) != ord('q'):
         color, depth = cpp.cd()
         dip.show_cd(color, depth, 'cd')
@@ -35,19 +45,21 @@ try:
             else:  # end of recording
                 # analyze data, give out result
 
-                # x = preprocessing(data)
-                # y = crnn(x)
-                # pred = classification(y)
-                # print(pred)
+                x = preprocess_seq(data[:int(0.9 * len(data))])
+                if len(x.shape) < 4:
+                    continue
+                x = torch.tensor(x.transpose(0, 3, 1, 2))
+
+                y = model(x).detach().numpy().argmax()
+                gesture = gesture_map_rev[y]
+                print(gesture)
                 # choice = input("add it to training set? (y/n)")
                 # if choice == 'y':
-                #     file_name = os.path.join(root_path, pred, str(int(time()))) + '.pkl'
-                #     f = open(file_name, 'wb')
-                #     pickle.dump((data[:int(0.9 * len(data))], gesture), file=f)
-                #     f.close()
+                #     file_name = os.path.join(root_path, gesture, str(int(time()))) + '.pkl'
+                #     with open(file_name, 'wb') as f:
+                #         pickle.dump((data[:int(0.9 * len(data))], gesture), file=f)
 
-                recording = False
-                print("Please show me your hand pose, q to quit!")
+                recording = False 
 
             continue
 
