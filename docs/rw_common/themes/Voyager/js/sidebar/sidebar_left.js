eb24939ca42b76bd7580776078333b883ba4bb99
KylinC <k1017856853@icloud.com>
2019-11-27 19:59:06 +0800

    add new docs webpage

---
+++ docs/rw_common/themes/Voyager/js/sidebar/sidebar_left.js
@@ -0,0 +1,3 @@
+
+elixir.sidebarLeft={};elixir.sidebarLeft=(function(){var jQuery=elixir.jQuery;var $=jQuery;var $elixir=jQuery.noConflict();function sidebarLeftFunction(){$('#sidebar').addClass('col-md-3 col-md-pull-9');$('#content').addClass('col-md-9 col-md-push-3');}
+$(document).ready(function(){sidebarLeftFunction();});})(elixir.sidebarLeft);
---
+++ docs/rw_common/themes/Voyager/js/sidebar/sidebar_right.js
@@ -0,0 +1,3 @@
+
+elixir.sidebarLeft={};elixir.sidebarLeft=(function(){var jQuery=elixir.jQuery;var $=jQuery;var $elixir=jQuery.noConflict();function sidebarLeftFunction(){$('#sidebar').addClass('col-md-3');$('#content').addClass('col-md-9');}
+$(document).ready(function(){sidebarLeftFunction();});})(elixir.sidebarLeft);
