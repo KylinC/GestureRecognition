cbe584a8fb48976ae5fe2de61596d9b02947ae68
Arthur-99 <1224795746@qq.com>
2019-12-03 22:44:14 +0800

    add erosion and dilation when cropping images

--- dip.py
+++ dip.py
@@ -17,10 +17,16 @@ def crop_cd(color, depth):
     gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
     ret, binary = cv2.threshold(gray, 0, 255,
                                 cv2.THRESH_BINARY | cv2.THRESH_TRIANGLE)
+
+    kernel = np.ones((5, 5), np.uint8)
+
+    binary = cv2.dilate(binary, kernel, iterations=4)
+    binary = cv2.erode(binary, kernel, iterations=4)
+
     bb = get_bb(binary)
 
     if bb is None:
-        return None, None
+        return None, None #, binary
 
     crop_color = get_crop_image(color, bb)
     crop_depth = get_crop_image(depth, bb)
@@ -28,4 +34,4 @@ def crop_cd(color, depth):
     resized_crop_color = cv2.resize(crop_color, (64, 64))
     resized_crop_depth = cv2.resize(crop_depth, (64, 64))
 
-    return resized_crop_color, resized_crop_depth
+    return resized_crop_color, resized_crop_depth # , binary
