ca035052d2a56e8e5dcbc5d1a0161a9346d18da3
Arthur-99 <1224795746@qq.com>
2019-12-06 11:57:47 +0800

    test files

---
+++ test/data_demo.py
@@ -0,0 +1,18 @@
+import pickle 
+import numpy as np 
+import cv2 
+import os
+
+root_path = './data/wipe_left'
+
+for file_name in os.listdir(root_path):
+    file_name = os.path.join(root_path, file_name)
+    f = open(file_name, 'rb')
+    seq, label = pickle.load(f)
+    for c, d in seq:
+        cv2.imshow('c', c)
+        cv2.waitKey(10)
+    f.close()
+
+    while cv2.waitKey(1) != ord('q'):
+        pass 
